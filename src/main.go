a3fdf13cc8ddcaef71d5ab196290ab8366f7da0f
samtcifihi <la.lojban@gmail.com>
2022-08-10 22:12:45 -0400

    comment

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	fmt.Println("Welcome to waving-hands")
 
+	// DEBUG
 	test_gamestate := gamestate.New_Game_State() // DEBUG
 	fmt.Println("DEBUG test_gamestate: ", test_gamestate)
 
